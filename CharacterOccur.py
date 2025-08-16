8dd6bb4e7aae1a3aad53ada35ce74bcaef0863ed
Sawmbawdy <saiompatro12@gmail.com>
2025-08-15 21:10:37 -0400

    Add files via upload

--- CharacterOccur.py
+++ CharacterOccur.py
@@ -1,9 +1,10 @@
-i = 1 
-
-for i in range(1, 6):
-    j = 1
-    for j in range(1, 11):
-        print(j,end='|')
-        j += 1
+Word = str(input("Type the word you want to scan: \n"))
+char = str(input("Type the character you want to find: \n"))
+count = 0   
+i = 0
+while i < len(Word):
+    if Word[i] == char:
+        count += 1
     i += 1
-    print()
+    
+print("The character", char, "appears", count, "times in the word", Word)
