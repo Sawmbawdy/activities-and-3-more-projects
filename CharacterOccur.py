7303bf2a5cfd343bdc384d3fd62c2bbc22e6e2c4
Sawmbawdy <saiompatro12@gmail.com>
2025-08-15 20:56:27 -0400

    Add files via upload

--- CharacterOccur.py
+++ CharacterOccur.py
@@ -1,9 +1,9 @@
 i = 1 
 
-while i <= 5:
+for i in range(1, 6):
     j = 1
-    while j <= 10:
-        print(j, end="")
+    for j in range(1, 11):
+        print(j,end='|')
         j += 1
     i += 1
     print()
