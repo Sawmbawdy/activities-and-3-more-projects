cb9ca8b7e31d38c93f76b85071d6f9288636617b
Sawmbawdy <saiompatro12@gmail.com>
2025-08-22 19:46:00 -0400

    Add files via upload

---
+++ TurtleStar.py
@@ -0,0 +1,28 @@
+import turtle
+board = turtle.Turtle()
+turtle.Screen().bgcolor("black")
+turtle.Screen().setup(800, 600)
+
+board.color("green")
+
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.penup()
+board.left(150)
+board.forward(50)
+board.pendown()
+
+board.left(90)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+turtle.done()
+
